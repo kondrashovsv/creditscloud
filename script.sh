57411acbb3b77fd699ea23d6592ba6113136f4c8
kondrashovsv <41994386+kondrashovsv@users.noreply.github.com>
2019-02-18 18:17:47 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,9 +13,13 @@ git clone https://gitlab.com/SK81/credits-ansible.git
 cd credits-ansible
 
 echo "[nodes]" > production
-echo "MyNode2 ansible_host=mynode2 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-echo "MyNode3 ansible_host=mynode3 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-echo "MyNode4 ansible_host=mynode4 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-echo "MyNode5 ansible_host=mynode5 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+# echo "MyNode2 ansible_host=mynode2 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+# echo "MyNode3 ansible_host=mynode3 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+# echo "MyNode4 ansible_host=mynode4 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+# echo "MyNode5 ansible_host=mynode5 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+echo "MyNode2 ansible_host=mynode2 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
+echo "MyNode3 ansible_host=mynode3 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
+echo "MyNode4 ansible_host=mynode4 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
+echo "MyNode5 ansible_host=mynode5 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
 echo "[signals]" >> production
-echo "MySignal1 ansible_host=localhost ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
+echo "MySignal1 ansible_host=localhost ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
