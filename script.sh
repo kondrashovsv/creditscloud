fdc33bb624f41cecb7c035fa521e87c5a5e8a431
kondrashovsv <41994386+kondrashovsv@users.noreply.github.com>
2019-02-27 19:46:29 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,10 +14,6 @@ sudo chown -R $1:$1 /tmp/dev/credits-ansible
 cd credits-ansible
 
 echo "[nodes]" > production
-# echo "MyNode2 ansible_host=mynode2 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-# echo "MyNode3 ansible_host=mynode3 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-# echo "MyNode4 ansible_host=mynode4 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
-# echo "MyNode5 ansible_host=mynode5 ansible_connection=ssh ansible_user={{vault_user}} ansible_ssh_pass={{vault_pass}}" >> production
 echo "MyNode1 ansible_host=mynode1 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
 echo "MyNode2 ansible_host=mynode2 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
 echo "MyNode3 ansible_host=mynode3 ansible_connection=ssh ansible_user=$1 ansible_ssh_pass=$2" >> production
